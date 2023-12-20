33db5697574bb2371e2f1b377e34ed0c733a117d
TharanginiGajendran <tharanginig1998@gmail.com>
2023-12-20 10:07:34 +0530

    commit2

--- app/src/main/java/com/example/activitylifecycle/MainActivity.kt
+++ app/src/main/java/com/example/activitylifecycle/MainActivity.kt
@@ -1,44 +1,60 @@
 package com.example.activitylifecycle
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import android.view.View.OnClickListener
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        Log.i("CheckActivityLifecycle","MainActivity: onCreated")
+        Log.i("MyTag","MainActivity: onCreated")
+
+        viewListeners()
     }
 
-    override fun onResume() {
-        super.onResume()
-        Log.i("CheckActivityLifecycle","MainActivity: onResumed")
-    }
+    private fun viewListeners(){
+        val myBtn = findViewById<Button>(R.id.next_btn)
+        myBtn.setOnClickListener(object : OnClickListener{
+            override fun onClick(p0: View?) {
+                val intent = Intent(this@MainActivity,SecondActivity::class.java)
+                startActivity(intent)
+            }
 
-    override fun onPause() {
-        super.onPause()
-        Log.i("CheckActivityLifecycle","MainActivity: onPaused")
+        })
+    }
+    override fun onStart() {
+        super.onStart()
+        Log.i("MyTag","MainActivity: onStarted")
     }
 
     override fun onRestart() {
         super.onRestart()
-        Log.i("CheckActivityLifecycle","MainActivity: onRestarted")
+        Log.i("MyTag","MainActivity: onRestarted")
     }
 
-    override fun onDestroy() {
-        super.onDestroy()
-        Log.i("CheckActivityLifecycle","MainActivity: onDestroyed")
+    override fun onResume() {
+        super.onResume()
+        Log.i("MyTag","MainActivity: onResumed")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.i("MyTag","MainActivity: onPaused")
     }
 
     override fun onStop() {
         super.onStop()
-        Log.i("CheckActivityLifecycle","MainActivity: onStopped")
+        Log.i("MyTag","MainActivity: onStopped")
     }
 
-    override fun onStart() {
-        super.onStart()
-        Log.i("CheckActivityLifecycle","MainActivity: onStarted")
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.i("MyTag","MainActivity: onDestroyed")
     }
 }
--- app/src/main/java/com/example/activitylifecycle/SecondActivity.kt
+++ app/src/main/java/com/example/activitylifecycle/SecondActivity.kt
@@ -2,10 +2,55 @@ package com.example.activitylifecycle
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.Toast
 
 class SecondActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
+
+        viewListeners()
+        Log.i("MyTag","SecondActivity: onCreated")
+
     }
+
+    private fun viewListeners(){
+        val okBtn = findViewById<Button>(R.id.second_btn)
+        okBtn.setOnClickListener {
+            Toast.makeText(this,"This is Second Activity",Toast.LENGTH_LONG).show()
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.i("MyTag","SecondActivity: onStarted")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.i("MyTag","SecondActivity: onRestarted")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.i("MyTag","SecondActivity: onResumed")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.i("MyTag","SecondActivity: onPaused")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.i("MyTag","SecondActivity: onStopped")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.i("MyTag","SecondActivity: onDestroyed")
+    }
+
 }
